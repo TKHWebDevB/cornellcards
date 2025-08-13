e3c539e1294e6f8d6e2d76aad3d8c8f7403d723c
ControlAltTea <controlalttea@gmail.com>
2025-08-12 18:35:18 -0700

    created axiosClient

---
+++ frontend/axios-instrance.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import supabase from "./client"
+
+const getAxiosClient = async () => {
+    const currentSession = await supabase.auth.getSession();
+
+    const instance = axios.create({
+        headers: {
+            Authorization: `Bearer ${currentSession.data.session.access_token}`
+        },
+    })
+
+    return instance;
+}
