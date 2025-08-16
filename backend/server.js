e08b73c5f5599ba53908e52c0bf55a7744bb1d2c
Alexis Gonzalez <alexis.gonzalez.390@gmail.com>
2025-08-15 17:59:31 -0700

    adding GET routes for initial dummy data

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
-// import todoRouter from "./routes/todo.js";
+import notebooksRouter from "./routes/notebooks.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
-// app.use("/notes", notesRouter);
+app.use("/collections", notebooksRouter);
+// app.use("/flashcards", flashCardRouter); (Just a GET route for the creation of flashcards)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
